93c42703a1034c9f253d2f05d9402105b0fa3c1a
Prajit Adhikari <57715196+adhikariprajitraj@users.noreply.github.com>
2020-03-29 09:00:22 +0545

    Euler problem 6
    easiest

---
+++ problem5.py
@@ -0,0 +1,21 @@
+def find_lcm(num1, num2): 
+    if(num1>num2): 
+        num = num1 
+        den = num2 
+    else: 
+        num = num2 
+        den = num1 
+    rem = num % den 
+    while(rem != 0): 
+        num = den 
+        den = rem 
+        rem = num % den 
+    gcd = den 
+    lcm = int(int(num1 * num2)/int(gcd)) 
+    return lcm 
+    
+lcm =find_lcm(1,2)
+for i in range(1,21):
+    lcm = find_lcm(lcm,i)
+print(lcm)
+    
